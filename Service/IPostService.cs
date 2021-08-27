4097bd380224110b8bffbbaa30787f6cbcc34a47
Jovan <jovanpop92@gmail.com>
2021-08-27 17:59:32 +0200

    Creating logics for wall posts functions

--- Service/IPostService.cs
+++ Service/IPostService.cs
@@ -1,12 +1,14 @@
-﻿using NistagramSQLConnection.Model;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using NistagramSQLConnection.Model;
 
 namespace NistagramSQLConnection.Service.Interface
 {
     public interface IPostService
     {
 
-        List<WallPost> GetAllWallPosts(bool isPublic);
+        List<WallPost> GetAllWallPosts(List<bool> isPublic, int page, int limit);
         List<Reaction> GetAllReactions(List<long> id);
+        bool PutReaction(long id, bool type, long userId);
+        WallPost NewPost(long userId, string description, string imgLink, bool isPublic);
     }
 }
